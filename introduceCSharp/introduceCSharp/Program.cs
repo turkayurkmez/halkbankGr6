a95bef32a6ffefbbd8277d207cac97f7c4d40da5
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-04-10 11:43:45 +0300

    Variables &  Types

---
+++ introduceCSharp/introduceCSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace introduceCSharp
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            if (true)
+            {
+                while (true)
+                {
+                    if (true)
+                    {
+                        while (true)
+                        {
+
+                            if (true)
+                            {
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
